822d832e00021ef507ebb8b0134374eec54811d1
Wei-Tse,Hsu <39806192+hsuweizte@users.noreply.github.com>
2023-05-22 14:15:03 +0800

    Update UserServiceImpl.java

--- src/main/java/com/hsuweizte/bloghsuweizte/service/UserServiceImpl.java
+++ src/main/java/com/hsuweizte/bloghsuweizte/service/UserServiceImpl.java
@@ -5,9 +5,6 @@ import com.hsuweizte.bloghsuweizte.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by limi on 2017/10/15.
- */
 @Service
 public class UserServiceImpl implements UserService {
 
