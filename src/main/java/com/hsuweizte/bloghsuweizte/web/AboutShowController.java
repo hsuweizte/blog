9e2fb705e51e1791b5bdb18bf091cf5c29184f22
hsuweizte <hsuweizte@gmail.com>
2023-07-01 00:50:48 +0800

    update

--- src/main/java/com/hsuweizte/bloghsuweizte/web/AboutShowController.java
+++ src/main/java/com/hsuweizte/bloghsuweizte/web/AboutShowController.java
@@ -2,9 +2,6 @@ package com.hsuweizte.bloghsuweizte.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Created by limi on 2017/10/24.
- */
 @Controller
 public class AboutShowController {
 
